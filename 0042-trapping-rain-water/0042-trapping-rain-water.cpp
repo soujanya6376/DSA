604d998818eed7bef5856c0d39a457c3b608d7c6
soujanya6376 <soujanyasree6376@gmail.com>
2026-06-20 22:47:22 +0530

    Time: 0 ms (100%), Space: 27.4 MB (6.62%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int total=0;
-        int len=height.size();
-        vector<int>prefix(len);
-        vector<int>suffix(len);
-        prefix[0]=height[0];
-        suffix[len-1]=height[len-1];
-        for(int i=1;i<height.size();i++){
-            prefix[i]=max(prefix[i-1],height[i]);
+        int n=height.size();
+        vector<int>leftmax(n);
+        vector<int>rightmax(n);
+        int left=0;
+        int right=n-1;
+        int tot=0;
+        leftmax[0]=height[0];
+        rightmax[n-1]=height[n-1];
+        for(int i=1;i<n;i++){
+            leftmax[i]=max(leftmax[i-1],height[i]);
+            left++;
         }
-        for(int i=len-2;i>=0;i--){
-            suffix[i]=max(suffix[i+1],height[i]);
+        for(int i=n-2;i>=0;i--){
+            rightmax[i]=max(rightmax[i+1],height[i]);
+            right--;
         }
-        for(int i=0;i<height.size();i++){
-            total+=min(prefix[i],suffix[i])-height[i];
+        for(int i=0;i<n;i++){
+          tot+=min(leftmax[i],rightmax[i])-height[i];
         }
-        return total;
+        return tot;
     }
 };
